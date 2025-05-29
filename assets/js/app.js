cefe8fc28868297f324c72448d04363460644c0c
Gustavo Leite <164664879+gusta-leite@users.noreply.github.com>
2025-05-29 18:27:47 -0300

    upload

---
+++ assets/js/app.js
@@ -0,0 +1,22 @@
+document.documentElement.style.cursor = 'none';
+
+// gsap to cursor
+gsap.set(".cursor", { xPercent: -50, yPercent: -50 });
+
+let xToCursor = gsap.quickTo(".cursor", "x", { duration: 0.4, ease: "power3" }),
+  yToCursor = gsap.quickTo(".cursor", "y", { duration: 0.4, ease: "power3" });
+
+window.addEventListener("mousemove", (e) => {
+  xToCursor(e.clientX);
+  yToCursor(e.clientY);
+});
+
+//
+const altura = document.querySelector('.lp-container').offsetHeight;
+document.querySelectorAll('.sidebar-right, .sidebar-left').forEach(elemento => {
+  elemento.style.height = `${altura}px`;
+});
+
+// change colors
+
+/* document.body.classList.add("dark"); */
